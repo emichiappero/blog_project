d36e0f65c6d0ada2d12987ced33cd2f261020359
emichiappero <emichiappero@gmail.com>
2024-07-26 21:06:59 -0300

    Bug fixes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,15 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-const firebaseConfig2 = {
-  apiKey: "AIzaSyBo4-_Dz47mS0ok9uS4HCre0X-0kg4bVZM",
-  authDomain: "blog-app-f4781.firebaseapp.com",
-  projectId: "blog-app-f4781blog-app-f4781.firebaseapp.com",
-  storageBucket: "blog-app-f4781.appspot.com",
-  messagingSenderId: "796923825071",
-  appId: "1:796923825071:web:8a8c74c1b2fadf52693d5a",
-};
-
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
